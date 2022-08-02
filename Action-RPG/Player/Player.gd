d4f64053cbc403ed5b8ce9d1c283463784663b02
aneylon <arlen.m.neylon@gmail.com>
2022-08-01 19:12:18 -0600

    Update player animations with tree.

--- Action-RPG/Player/Player.gd
+++ Action-RPG/Player/Player.gd
@@ -7,7 +7,8 @@ const FRICTION = 10
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
-
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 #func _ready():
 #	animationPlayer = $AnimationPlayer
 	
@@ -17,16 +18,21 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
+	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		else: 
-			animationPlayer.play("RunLeft")
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+#		if input_vector.x > 0:
+#			animationPlayer.play("RunRight")
+#		else: 
+#			animationPlayer.play("RunLeft")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION )
 #		velocity +=  input_vector * ACCELERATION * delta
 #		velocity = velocity.clamped(MAX_SPEED)
 	else:
-		animationPlayer.play("IdleDown")
+#		animationPlayer.play("IdleDown")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO,  FRICTION )
 #		velocity = Vector2.ZERO
 	
